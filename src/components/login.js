3d5878fbc2c87d7eb0b46cf214167aac9ce60dee
Nizamul Haque <haniza@microsoft.com>
2016-06-16 12:32:05 +0530

    login screen with login input

--- src/components/login.js
+++ src/components/login.js
@@ -3,17 +3,46 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  TextInput,
+  TouchableHighlight,
   StyleSheet
 } from 'react-native';
 
 class Login extends Component {
-  render() {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: ''			
+		};
+	}
+	
+	onPress() {
+		console.log(this.state.username);
+	}
+	render() {
     return (
-      <View style={styles.container}>
-        <Text style={{color: 'white'}}>Dummy Text!</Text>
+       <View style={styles.container}>
+        <View style={styles.loginContainer}>
+          <TextInput
+            style={styles.input}
+            value={this.state.username}
+            onChangeText={(text) => this.setState({username: text})}
+            placeholder={'Enter User name'}
+            maxLength={12}
+            multiline={false}
+            />
+ 
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor={'#328FE6'}
+            onPress={this.onPress}
+            >
+            <Text style={styles.label}>LOGIN</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
-  }
+	}
 }
  
 var styles = StyleSheet.create({
@@ -22,6 +51,41 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'stretch',
     backgroundColor: '#6E5BAA'
+  },
+  loginContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: 250,
+    color: '#555555',
+    padding: 10,
+    height: 50,
+    borderColor: '#32C5E6',
+    borderWidth: 1,
+    borderRadius: 4,
+    alignSelf: 'center',
+    backgroundColor: '#ffffff'
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#328FE6',
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: '#32c5e6'
+  },
+  label: {
+    width: 230,
+    flex: 1,
+    alignSelf: 'center',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#ffffff'
   }
 });
 
