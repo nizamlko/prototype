b8f8b623fdfff155ac3fe7995b0b136225b58596
Nizamul Haque <haniza@microsoft.com>
2016-06-16 16:05:15 +0530

    chat view

--- src/components/chat.js
+++ src/components/chat.js
@@ -3,47 +3,70 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  StyleSheet,
-  TouchableHighlight
+  TouchableHighlight,
+  TextInput,
+  Dimensions,
+  StyleSheet
 } from 'react-native';
  
 var sendbird = require('sendbird');
- 
+var windowSize = Dimensions.get('window');
 
 class Chat extends Component{
 	constructor(props) {
 		super(props);
+		this.state = {
+			message:''
+		}
 	}
 	
 	render() {
     
 	return (
-        <View style={styles.container}>
-          <View style={styles.topContainer}>
-            <TouchableHighlight
-              underlayColor={'#4e4273'}
-              onPress={this.onBackPress.bind(this)}
-              style={{marginLeft: 15}}
-              >
-              <Text style={{color: '#fff'}}>&lt; Back</Text>
-            </TouchableHighlight>
-          </View>
-          <View style={styles.chatContainer}>
-            <Text style={{color: '#000'}}>Chat</Text>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text style={{color: '#000'}}>Input</Text>
-          </View>
-        </View>
+		   <View style={styles.container}>
+			<View style={styles.topContainer}>
+			  <TouchableHighlight
+				underlayColor={'#4e4273'}
+				onPress={this.onBackPress}
+				style={{marginLeft: 15}}
+				>
+				<Text style={{color: '#fff'}}>&lt; Back</Text>
+			  </TouchableHighlight>
+			</View>
+			<View style={styles.chatContainer}>
+			  <Text style={{color: '#000'}}>Chat</Text>
+			</View>
+			<View style={styles.inputContainer}>
+			  <View style={styles.textContainer}>
+				<TextInput
+				  style={styles.input}
+				  value={this.state.message}
+				  onChangeText={(text) => this.setState({message: text})}
+				  />
+			  </View>
+			  <View style={styles.sendContainer}>
+				<TouchableHighlight
+				  underlayColor={'#4e4273'}
+				  onPress={() => this.onSendPress()}
+				  >
+				  <Text style={styles.sendLabel}>SEND</Text>
+				</TouchableHighlight>
+			  </View>
+			</View>
+		  </View>
 		);
 	}
 	
 	onBackPress() {
       this.props.navigator.pop();
     }
+	
+	onSendPress() {
+       console.log(this.state.message);
+	   this.setState({message: ''});
+    }
 }
- 
-var styles = StyleSheet.create({
+ var styles = StyleSheet.create({
     container: {
       flex: 1,
       justifyContent: 'center',
@@ -65,9 +88,36 @@ var styles = StyleSheet.create({
     },
     inputContainer: {
       flex: 1,
-      justifyContent: 'center',
-      alignItems: 'stretch'
-    }
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      backgroundColor: '#6E5BAA'
+    },
+    textContainer: {
+      flex: 1,
+      justifyContent: 'center'
+    },
+    sendContainer: {
+      justifyContent: 'flex-end',
+      paddingRight: 10
+    },
+    sendLabel: {
+      color: '#ffffff',
+      fontSize: 15
+    },
+    input: {
+      width: windowSize.width - 70,
+      color: '#555555',
+      paddingRight: 10,
+      paddingLeft: 10,
+      paddingTop: 5,
+      height: 32,
+      borderColor: '#6E5BAA',
+      borderWidth: 1,
+      borderRadius: 2,
+      alignSelf: 'center',
+      backgroundColor: '#ffffff'
+    },
   });
 
 module.exports =Chat;
